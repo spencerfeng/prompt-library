abcd4d644a79cdc0b5e2d055e3c2c2e804f2d563
Spencer Feng <spencer.feng@kindredgroup.com>
2026-07-02 10:21:34 +1000

    style: add some eslint rules for consistent styling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
@@ -17,9 +17,18 @@ const eslintConfig = [
       ".next/**",
       "out/**",
       "build/**",
-      "next-env.d.ts",
-    ],
+      "next-env.d.ts"
+    ]
   },
-];
+  {
+    rules: {
+      "semi": ["error", "never"],
+      "quotes": ["error", "double"],
+      "comma-dangle": ["error", "never"],
+      "arrow-parens": ["error", "always"],
+      "object-curly-spacing": ["error", "always"]
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
